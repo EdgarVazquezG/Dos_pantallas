9facc60ef20caa31b3f7eef14c690ed6592757d1
D310820 <tecnocel30210@gmail.com>
2026-05-29 15:05:38 -0600

    Initial Commit

---
+++ Practica_2_Pantalla1_Pantalla2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practica_2_Pantalla1_Pantalla2
+//
+//  Created by Jose Daniel Espinoza Gomez on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Practica_2_Pantalla1_Pantalla2/Practica_2_Pantalla1_Pantalla2App.swift
@@ -0,0 +1,17 @@
+//
+//  Practica_2_Pantalla1_Pantalla2App.swift
+//  Practica_2_Pantalla1_Pantalla2
+//
+//  Created by Jose Daniel Espinoza Gomez on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Practica_2_Pantalla1_Pantalla2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
